bb350c72c6256223ac9e08be77a1d636275b8a55
Reishi Mitani <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2022-11-29 17:34:56 +0000

    add gcc configs

---
+++ day3/main.go
@@ -0,0 +1,11 @@
+
+		Hckage main
+
+func io_hlt();
+
+func main() {
+fin:
+	io_hlt()
+	goto fin
+}
+LT
---
+++ day3/nasmfunc.asm
@@ -0,0 +1,24 @@
+kfunc
+; TAB=4
+
+;[FORMAT "WOOFF"]							; オブジェクトファイルを作るモード
+;[BITS 32]									; 32ビットモード用の機械語を作らせる
+
+; オブジェクトファイルのための情報
+
+;[FILE "naskfunc.nas"]						; ソースファイル名情報
+
+;		GLOBAL _io_hlt						; このプログラムに含まれる関数名
+
+; 以下は実際の関数
+
+;[SECTION .text] 							; オブジェクトファイルではこれを書いてからプログラムを書く
+section .data
+section .text
+GLOBAL main.io_hlt
+
+main.io_hlt:	; void io_hlt(void);
+		mov rax,0
+		RET
+
+
