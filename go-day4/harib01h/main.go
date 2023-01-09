af8ad8a27349e369b98e58312f5681e875013f17
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2023-01-09 16:52:19 +0900

    fix color

--- go-day4/harib01h/main.go
+++ go-day4/harib01h/main.go
@@ -6,9 +6,9 @@ import (
 
 const (
 	fbPhysAddr  uintptr = 0xa0000
-	COL8_000000 uint16  = 0
-	COL8_FF0000 uint16  = 1
-	COL8_00FF00 uint16  = 2
+	COL8_000000 uint16  = 0 // black
+	COL8_FF0000 uint16  = 1 // blue
+	COL8_00FF00 uint16  = 2 // green
 	COL8_FFFF00 uint16  = 3
 	COL8_0000FF uint16  = 4
 	COL8_FF00FF uint16  = 5
@@ -17,30 +17,30 @@ const (
 	COL8_C6C6C6 uint16  = 8
 	COL8_840000 uint16  = 9
 	COL8_008400 uint16  = 10
-	COL8_848400 uint16  = 11
+	LIGHTBLUE   uint16  = 11 // light blue
 	COL8_000084 uint16  = 12
 	COL8_840084 uint16  = 13
-	COL8_008484 uint16  = 14
-	COL8_848484 uint16  = 15
+	YELLOW      uint16  = 14 // yellow
+	WHITE       uint16  = 15 // white
 )
 
 func main() {
 	delay(1000)
 	xsize, ysize := 320, 200
-	boxFill8(xsize, 0, 0, xsize-1, ysize-29, COL8_008484)
+	boxFill8(xsize, 0, 0, xsize-1, ysize-29, LIGHTBLUE)
 	boxFill8(xsize, 0, ysize-28, xsize-1, ysize-28, COL8_C6C6C6)
 	boxFill8(xsize, 0, ysize-27, xsize-1, ysize-27, COL8_FFFFFF)
-	boxFill8(xsize, 0, ysize-26, xsize-1, ysize-1, COL8_C6C6C6)
+	boxFill8(xsize, 0, ysize-26, xsize-1, ysize-1, COL8_FFFF00)
 
 	boxFill8(xsize, 3, ysize-24, 59, ysize-24, COL8_FFFFFF)
 	boxFill8(xsize, 2, ysize-24, 2, ysize-4, COL8_FFFFFF)
-	boxFill8(xsize, 3, ysize-4, 59, ysize-4, COL8_848484)
-	boxFill8(xsize, 59, ysize-23, 59, ysize-5, COL8_848484)
+	boxFill8(xsize, 3, ysize-4, 59, ysize-4, WHITE)
+	boxFill8(xsize, 59, ysize-23, 59, ysize-5, WHITE)
 	boxFill8(xsize, 2, ysize-3, 59, ysize-3, COL8_000000)
 	boxFill8(xsize, 60, ysize-24, 60, ysize-3, COL8_000000)
 
-	boxFill8(xsize, xsize-47, ysize-24, xsize-4, ysize-24, COL8_848484)
-	boxFill8(xsize, xsize-47, ysize-23, xsize-47, ysize-4, COL8_848484)
+	boxFill8(xsize, xsize-47, ysize-24, xsize-4, ysize-24, WHITE)
+	boxFill8(xsize, xsize-47, ysize-23, xsize-47, ysize-4, WHITE)
 	boxFill8(xsize, xsize-47, ysize-3, xsize-4, ysize-3, COL8_FFFFFF)
 	boxFill8(xsize, xsize-3, ysize-24, xsize-3, ysize-3, COL8_FFFFFF)
 
