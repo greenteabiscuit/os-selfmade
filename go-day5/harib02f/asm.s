4ea2104c8a9f711f2b762f3cccc9454a22f3bfe8
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2023-01-14 10:40:41 +0900

    push and pop seem to work

--- go-day5/harib02f/asm.s
+++ go-day5/harib02f/asm.s
@@ -6,12 +6,12 @@ TEXT ·add(SB),$0-0
     MOVB AX, ret+4(FP) // 計算結果を戻り値として返す
 
     SUBL $24, SP    // neg関数の引数と戻り値サイズ+BPレジスタの退避先を確保
-    // MOVB BP, 16(SP) // 現在のBPレジスタをpush
+    MOVB BP, 16(SP) // 現在のBPレジスタをpush
     // LEAQ 16(SP), BP // BPレジスタを新しいスタックに更新
     // MOVB AX, (SP)   // 最初の引数iを渡す
     // CALL ·sub(SB)   // main.negを呼ぶ
     // MOVB 8(SP), AX  // main.negの戻り値をAXレジスタに取り出す
-    // MOVB 16(SP), BP // 退避していたBPレジスタをpop
+    MOVB 16(SP), BP // 退避していたBPレジスタをpop
     ADDL $24, SP    // スタックサイズを戻す
     // MOVB AX, ret2+12(FP) // 2番目の戻り値として返す
     RET
